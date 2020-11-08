7cf2fd2a7ed7025d1cb6fb1139cb08ef467bf512
Nidxegg <petr.luchinskiy@gmail.com>
2020-11-08 21:15:46 +0600

    add username

--- fixture/session.py
+++ fixture/session.py
@@ -24,7 +24,12 @@ class SessionHelper:
 
     def is_logged_as(self, username):
         driver = self.app.driver
-        return driver.find_element(By.XPATH, "//div/div[1]/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+
+    def get_logged_user(self):
+        driver = self.app.driver
+        return driver.find_element(By.XPATH, "//div/div[1]/form/b").text[1:-1]
 
     def is_logged_in(self):
         driver = self.app.driver
